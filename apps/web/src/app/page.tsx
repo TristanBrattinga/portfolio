a761615d3a2ce90d97f90a60f2a20018162373a0
TristanBrattinga <tristan.brattinga@gmail.com>
2024-02-01 23:35:58 +0100

    added real age

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,23 +1,12 @@
 import ProjectList from "ui/components/ProjectList"
 import Button from "ui/components/Button"
 import Link from "next/link"
+import Introduction from "ui/components/Introduction"
 
 export default function Page() {
     return (
         <div className="my-40">
-            <section className="container text-white flex flex-col items-center gap-4 my-20">
-                <h1 className="text-2xl md:text-5xl">Hi, my name is Tristan and I&apos;m 24 years old</h1>
-                <p className="text-center max-w-2xl">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                    culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-                <Button as={Link} href="/work" variant="primary">
-                    See all Projects
-                </Button>
-            </section>
+            <Introduction />
             <ProjectList
                 projects={[
                     { image: "/images/stock.jpg", title: "Project", date: new Date(), description: "Test test" },
---
+++ packages/ui/components/Introduction/index.tsx
@@ -0,0 +1,63 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import Button from "../Button"
+import Link from "next/link"
+import clsx from "clsx"
+import { slideInTop, useAnimations } from "../../utils/animations"
+
+const Introduction = ({}) => {
+    const { ref, inView } = useAnimations()
+    const [age, setAge] = useState(0)
+    const [isHovered, setIsHovered] = useState(false)
+
+    useEffect(() => {
+        const birthday = new Date("1999-07-11")
+        const today = new Date()
+
+        let updateAge = today.getFullYear() - birthday.getFullYear() - 1
+
+        if (today.getMonth() === birthday.getMonth() && today.getDay() === birthday.getDay()) {
+            updateAge += 1
+        }
+
+        setAge(updateAge)
+    }, [age])
+
+    return (
+        <section className="container text-white flex flex-col items-center gap-4 my-20">
+            <div
+                className={clsx("bg-white py-2 px-4 rounded-md absolute", slideInTop(isHovered), {
+                    "!-translate-y-[10px]": !isHovered,
+                })}
+            >
+                <p className="text-black font-medium">Yes this updates!</p>
+                <span className="arrow-down"></span>
+            </div>
+
+            <h1 className="text-2xl md:text-4xl text-center">
+                Hi, my name is Tristan and I&apos;m{" "}
+                <div
+                    onMouseEnter={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)}
+                    className="text-2xl md:text-4xl"
+                >
+                    {age}
+                </div>{" "}
+                years old
+            </h1>
+            <p className="text-center max-w-2xl">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum.
+            </p>
+            <Button as={Link} href="/work" variant="primary">
+                See all Projects
+            </Button>
+        </section>
+    )
+}
+
+export default Introduction
