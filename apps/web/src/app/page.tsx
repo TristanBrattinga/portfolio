e683b04d3e06191f10ad1062f1e138113c8cf296
TristanBrattinga <tristan.brattinga@gmail.com>
2024-04-10 20:26:17 +0200

    updates

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,11 @@
+import Cursor from "ui/components/Cursor"
+
 export default function Page() {
     return (
         <div className="flex justify-center items-center h-screen">
-            <h1 className="font-ailerons text-white text-3xl md:text-6xl lg:text-[100px] comingSoon">Coming soon...</h1>
+            <h1 className="font-ailerons text-white text-3xl md:text-6xl lg:text-[100px] comingSoon">
+                Coming soon...{" "}
+            </h1>
         </div>
     )
 }
